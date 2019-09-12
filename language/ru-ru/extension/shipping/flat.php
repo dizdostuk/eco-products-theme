99403501f7792dc45155c57fbd131dcd95dd699d
Ulanbek Zhusupov <diz.dostuk@yandex.ru>
2019-09-12 22:34:57 +0300

    Added date functionality and styled popup product for xs and sm devices

--- language/ru-ru/extension/shipping/flat.php
+++ language/ru-ru/extension/shipping/flat.php
@@ -1,6 +1,14 @@
 <?php
 // Text
 $_['text_title']       = 'Фиксированная стоимость доставки';
-$_['text_description'] = 'Доставка в следующий вторник ' . date('d.m.Y', strtotime('next tuesday'));
+
+
+if(date("D", time()) == "Tue") {
+  $date = new DateTime();
+  date_add($date, date_interval_create_from_date_string('8 days'));
+  $_['text_description'] = 'Доставка на следующий вторник ' . date_format($date, 'Y-m-d');
+} else {
+  $_['text_description'] = 'Доставка на следующий вторник ' . date('d-m-Y', strtotime('next tuesday'));
+}
 
 
--- language/ru-ru/extension/shipping/pickup.php
+++ language/ru-ru/extension/shipping/pickup.php
@@ -1,5 +1,12 @@
 <?php
 // Text
 $_['text_title']       = 'Самовывоз';
-$_['text_description'] = 'Доставка в следующую субботу ' . date('d.m.Y', strtotime('next saturday'));
+
+if(date("D", time()) == "Fri") {
+  $date = new DateTime();
+  date_add($date, date_interval_create_from_date_string('8 days'));
+  $_['text_description'] = 'Доставка на следующую субботу ' . date_format($date, 'Y-m-d');
+} else {
+  $_['text_description'] = 'Доставка на следующую субботу ' . date('d-m-Y', strtotime('next saturday'));
+}
 
